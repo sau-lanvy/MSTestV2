86f7c6bd208c358d0af9719497931ad9422cf8de
sau-lanvy <havanit@gmail.com>
2017-03-03 00:37:13 +0700

    Add source for simple mstest

---
+++ MSTestSample/MSTestSample.Test/SampleTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestSample.Test
+{
+    [TestClass]
+    public class SampleTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3)]
+        [DataRow(2, 2, 4)]
+        [DataRow(3, 2, 6)]
+        public void RowTest(int a, int b, int result)
+        {
+            Assert.AreEqual(result, a + b);
+        }
+    }
+}
